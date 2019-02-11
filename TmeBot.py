313196ce1e16137f00e7218f711053b51efb3b0c
Rustam-Deniz Emirali <46903429+rustamqua@users.noreply.github.com>
2019-02-12 01:46:08 +0600

    TmeBot
    Getting Started

---
+++ TmeBot.py
@@ -0,0 +1,13 @@
+import logging
+import telegram
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+updater = Updater(token='777912703:AAFrmem2Wo2L2ae-bUNCWz6hHf0vFae6C5g')
+dispatcher = updater.dispatcher
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Hey, you wanna play? ")
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+updater.start_polling()
