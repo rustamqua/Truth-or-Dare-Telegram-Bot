73c24c5f3e5be68ab3125dc53291769471b841ac
rustamqua <180107118@stu.sdu.edu.kz>
2019-03-21 05:26:25 +0600

    <feature><add base functions>

--- TmeBot.py
+++ TmeBot.py
@@ -1,29 +1,48 @@
-<<<<<<< HEAD
-import logging
-import telegram
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-updater = Updater(token='777912703:AAFrmem2Wo2L2ae-bUNCWz6hHf0vFae6C5g')
-dispatcher = updater.dispatcher
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
-def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Hey, you wanna play? ")
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-updater.start_polling()
-=======
-import logging
-import telegram
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-updater = Updater(token='777912703:AAFrmem2Wo2L2ae-bUNCWz6hHf0vFae6C5g')
-dispatcher = updater.dispatcher
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
-def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Hey, you wanna play? ")
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-updater.start_polling()
->>>>>>> 6743bc3b76055f98e075176fee5048590bae2221
+import telebot
+import constants
+import BotType
+import random
+bot= telebot.TeleBot(token=constants.token)
+new=BotType.Game()
+@bot.message_handler(commands=["start"])
+def welcome(message):
+    user_markup=telebot.types.ReplyKeyboardMarkup(True)
+    user_markup.row("/girls","/guys")
+    user_markup.row("/friends","/flirt")
+    user_markup.row("/adults")
+    user_markup.row("/play")
+    bot.reply_to(message,constants.hello,reply_markup=user_markup)
+@bot.message_handler(commands=["adults"])
+def adults(message):
+    new.setAdults()
+    bot.reply_to(message,"Adults mode setted")
+@bot.message_handler(commands=["friends"])
+def friends(message):
+    new.setFriends()
+    bot.reply_to(message,"Frinds mode setted")
+@bot.message_handler(commands=["girls"])
+def girls(message):
+    new.setGirls()
+    bot.reply_to(message,"Girls mode setted")
+@bot.message_handler(commands=["guys"])
+def guys(message):
+    new.setGuys()
+    bot.reply_to(message,"Guys mode setted")
+@bot.message_handler(commands=["flirt"])
+def flirt(message):
+    new.setFlirt()
+    bot.reply_to(message,"Flirt mode setted")
+@bot.message_handler(commands=["basic"])
+def basic(message):
+    new.setBasic()
+@bot.message_handler(commands=["play"])
+def play(message):
+    r=random.randrange(0,2)
+    if r==0:
+        r1=random.randrange(0,len(new.Game_Truth))
+        bot.send_message(message.chat.id,"Truth! "+new.Game_Truth[r1])
+    else:
+        r1=random.randrange(0,len(new.Game_Dare))
+        bot.send_message(message.chat.id,"Dare! "+new.Game_Dare[r1])
+
+bot.polling()
